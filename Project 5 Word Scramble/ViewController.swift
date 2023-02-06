99db363afe8b500b0e6221ccb2fd6d097a26fee9
rezakoushki95 <rezaaarhus@hotmail.com>
2023-02-07 00:25:21 +0100

    Add an "add" rightbarbutton to allow the user to submit words.

--- Project 5 Word Scramble/ViewController.swift
+++ Project 5 Word Scramble/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UITableViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
+		navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+		
 		if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
 			if let startsWords = try? String(contentsOf: startWordsURL) {
 				allWords = startsWords.components(separatedBy: "\n")
@@ -42,6 +44,23 @@ class ViewController: UITableViewController {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
 		cell.textLabel?.text = usedWords[indexPath.row]
 		return cell
+	}
+	
+	@objc func promptForAnswer() {
+		let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+		ac.addTextField()
+		
+		let sumbitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] _ in
+			guard let answer = ac?.textFields?[0].text else {return}
+			self?.submit(answer)
+		}
+		
+		ac.addAction(sumbitAction)
+		present(ac, animated: true)
+	}
+	
+	func submit(_ answer: String) {
+		
 	}
 }
 
